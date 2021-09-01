2e064610c9efacd2fe614335b03aa46ba3171076
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-09-01 08:44:46 -0500

    part paths

--- js.js
+++ js.js
@@ -3,7 +3,25 @@ const URL = "https://api.fda.gov/drug/event.json?api_key=";
 const API_Key = "IKBoKPvacjAy2yH0yYSHQUldCTmPaC1AMa6SPpgL";
 const URLandKey = `https://api.fda.gov/drug/event.json?api_key=${API_Key}}&search=`;
 
-console.log(URLandKey + "gabapentin");
+// brand name
+// =patient.drug.openfda.brand_name:""
+
+// generic name
+// =patient.drug.openfda.generic_name:""
+
+// usage
+// drugindication
+
+// side effects
+//  =patient.drug.openfda.generic_name:""+AND+patient.reaction.reactionmeddrapt:""
+
+// date
+// transmissiondate
+
+// manufacturer
+// =patient.drug.openfda.generic_name:""+AND+patient.drug.openfda.manufacturer_name:""
+
+
 
 const searchButton = document.querySelector("#search");
 
@@ -21,6 +39,13 @@ async function searchRX() {
   }
 }
 
-function searchRX{
+function searchRX(){
 
 };
+
+
+// select button
+// attach event listener
+// send axios get request to URL
+// append data to the page
+// 
