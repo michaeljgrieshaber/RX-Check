eb3d085d42bd2345a558715dde1a38a857d0990f
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-09-02 13:47:18 -0500

    JS done

--- js.js
+++ js.js
@@ -1,22 +1,29 @@
 // alert("Do not rely on Check RX to make decisions regarding medical care. While we make every effort to ensure that data is accurate, you should assume all results are unvalidated.");
 const URL = "https://api.fda.gov/drug/event.json?api_key=";
 const API_Key = "IKBoKPvacjAy2yH0yYSHQUldCTmPaC1AMa6SPpgL";
-// let userInput = "atorvastatin";
-// let userInput = "sudafed";
-// let userInput = "metformin";
 let userInput = document.querySelector("#user-input");
 console.log(userInput);
-
-// let userInput = getUserInput();
 const URLandKey = `https://api.fda.gov/drug/event.json?api_key=${API_Key}&search=`;//${userInput}`;
 
 
 
+let brand = document.getElementById("name-brand-result");
+let indication = document.getElementById("usage-result");
+let generic = document.getElementById("name-generic-result");
+let reaction = document.getElementById("side-effects-result");
+let date = document.getElementById("date-available-result");
+let manufacturer = document.getElementById("manufacturer-result");
 
-// function clearData() {
-//   results.innerHTML === "";
-//  }
 
+function clearData() {
+  brand.innerHTML = "";
+  generic.innerHTML = "";
+  indication.innerHTML = "";
+  reaction.innerHTML = "";
+  date.innerHTML = "";
+  manufacturer.innerHTML = "";
+
+ }
 
 const searchButton = document.querySelector("#search");
 searchButton.addEventListener("click", searchRX);
@@ -25,32 +32,24 @@ async function searchRX() {
   try {
     let res = await axios.get(`${URLandKey}${userInput.value}`);
  
+    clearData();
 
-    // let results = document.getElementsByClassName(".result");
-    // clearData();
-
-    let brand = document.getElementById("name-brand-result");
-    let textBrand = document.createTextNode(res.data.results[0].patient.drug[0].openfda.brand_name[0]);
+    let textBrand = document.createTextNode(res.data.results[0].patient.drug[0].openfda.brand_name[0] ? res.data.results[0].patient.drug[0].openfda.brand_name[0] : "no data" );
     brand.appendChild(textBrand);
 
-    let generic = document.getElementById("name-generic-result");
-    let textGeneric = document.createTextNode(res.data.results[0].patient.drug[0].openfda.generic_name[0]);
+    let textGeneric = document.createTextNode(res.data.results[0].patient.drug[0].openfda.generic_name[0] ? res.data.results[0].patient.drug[0].openfda.generic_name[0] : "no data" );
     generic.appendChild(textGeneric);
 
-    let indication = document.getElementById("usage-result");
-    let textIndication = document.createTextNode(res.data.results[0].patient.drug[0].drugindication);
+    let textIndication = document.createTextNode(res.data.results[0].patient.drug[0].drugindication ? res.data.results[0].patient.drug[0].drugindication : "no data" );
     indication.appendChild(textIndication);
 
-    let reaction = document.getElementById("side-effects-result");
-    let textReaction = document.createTextNode(res.data.results[0].patient.reaction[0].reactionmeddrapt);
+    let textReaction = document.createTextNode(res.data.results[0].patient.reaction[0].reactionmeddrapt ? res.data.results[0].patient.reaction[0].reactionmeddrapt : "no data" );
     reaction.appendChild(textReaction);
 
-    let date = document.getElementById("date-available-result");
-    let textDate = document.createTextNode(res.data.results[0].transmissiondate);
+    let textDate = document.createTextNode(res.data.results[0].transmissiondate ? res.data.results[0].transmissiondate : "no data" );
     date.appendChild(textDate);
 
-    let manufacturer = document.getElementById("manufacturer-result");
-    let textManufacturer = document.createTextNode(res.data.results[0].patient.drug[0].openfda.manufacturer_name[0]);
+    let textManufacturer = document.createTextNode(res.data.results[0].patient.drug[0].openfda.manufacturer_name[0] ? res.data.results[0].patient.drug[0].openfda.manufacturer_name[0] : "no data" );
     manufacturer.appendChild(textManufacturer);
 
   } catch (error) {
